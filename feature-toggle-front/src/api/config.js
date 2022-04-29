47f4b54359801a8523cf6fad254019372a5f2863
gabrueks <gabriel03899@hotmail.com>
2022-04-29 12:04:01 -0300

    broke

--- feature-toggle-front/src/api/config.js
+++ feature-toggle-front/src/api/config.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-export const API_URL = 'http://localhost:8080';
+export const API_URL = 'https://feature-toggle-backend.herokuapp.com/';
 
 const apiInstance = axios.create({ baseURL: API_URL, timeout: 30000 });
 
